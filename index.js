e5d15fe6e72127776f209f20f357c90bfdd30afe
gkavin57 <gkavin77@gmail.com>
2023-01-09 19:50:01 +0530

    try

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+require("./src/Connection/Connection");
+const noteRouter = require("./src/Routes/noteRouter");
+const userRouter = require("./src/Routes/userRouter");
+
+//Middleware;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//Routes;
+app.use("/users", userRouter);
+app.use("/api/notes", noteRouter);
+
+//Port Listing;
+const PORT = 7000;
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Port is Running on " + PORT);
+});
+
+app.get("/", (req, res) => {
+  console.log("hello 13");
+});
